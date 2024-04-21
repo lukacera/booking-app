249b10a3727ce6bb97c403278913765acea4f4a2
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-04-21 13:31:47 +0200

    Update cardPopularLocation, add scaling when user hovers over image

--- src/app/components/CardPopularLocation.tsx
+++ src/app/components/CardPopularLocation.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+"use client"
+import React, { useRef } from "react"
 import Image from "next/image";
 import { Kalam } from "next/font/google";
 
@@ -6,23 +7,37 @@ import { Kalam } from "next/font/google";
 const kalam = Kalam({ subsets: ["latin"], weight: "400" })
 
 const CardPopularLocation: React.FC<{ location: any }> = ({ location }) => {
+    const imageRef = useRef<HTMLImageElement>(null)
+
+    const handleHover = () => {
+        if (imageRef.current) {
+            imageRef.current.classList.add("scale-125", "overflow-hidden")
+        }
+    }
+    const handleMouseLeave = () => {
+        if (imageRef.current) {
+            imageRef.current.classList.remove('scale-125', 'overflow-hidden');
+        }
+    };
     return (
         <div className="flex relative
-    w-[30rem] h-[20rem]">
+    w-[30rem] h-[20rem] overflow-hidden">
             <Image src={location.image} alt=""
                 width={400} height={400}
-                className="w-full h-full opacity-95 rounded-xl" />
+                className="w-full h-full opacity-95 rounded-xl
+                duration-500" ref={imageRef} />
 
             {/* Overlay */}
             <div className="absolute inset-0 bg-gradient-to-b 
-      from-transparent to-black rounded-lg"></div>
+      from-transparent to-black rounded-lg cursor-pointer"
+                onMouseEnter={handleHover}
+                onMouseLeave={handleMouseLeave}></div>
 
             <div className="absolute bottom-5 right-5 flex 
       items-center gap-3">
                 <Image src={location.flag} alt=""
                     height={200} width={200} className="w-[3.5rem] aspect-square
-          rounded-full"
-                />
+          rounded-full"  />
                 <span className={`text-white ${kalam.className}
         text-xl font-bold`}>
                     {location.place}
