24e8466e61f73188b182af1f2bf58e97fc483958
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-04-21 17:38:13 +0200

    Update header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,27 @@
-import React from "react"
+"use client"
+import React, { useState } from "react";
 import Logo from "./Logo";
+import { FaLocationDot } from "react-icons/fa6";
+
 const Header: React.FC = () => {
     return (
-        <header className="fixed top-0 right-0 w-full 
-        bg-brown_1 px-20 py-6 opacity-95 z-10">
-            <div className="max-w-[10%]">
-                <Logo />
-            </div>
+        <header className="relative">
+            <div className="fixed top-0 right-0 w-full bg-brown_1 px-20 z-10
+            h-[8.5rem] grid place-items-center">
+                <div className="flex justify-start absolute top-7 left-20">
+                    <Logo />
+                </div>
+                <div className="flex items-center gap-4 bg-orange_1 py-4 px-3
+                rounded-lg">
+                    <FaLocationDot className="text-gold_1 text-[1.2rem]" />
+                    <input type="text" className="bg-transparent
+                        outline-none text-white placeholder:text-white"
+                        spellCheck={false} placeholder="Where do you want to go?" />
+                </div>
 
+            </div>
         </header>
-    )
+    );
 };
 
 export default Header;
