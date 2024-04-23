b0538c14c024d93345d6a5063da6f54588b95d79
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-04-23 21:35:04 +0200

    Hotel helper function and hotel type

---
+++ types/HotelType.ts
@@ -0,0 +1,17 @@
+export type HotelType = {
+    chainCode: string;
+    iataCode: string;
+    dupeId: number;
+    name: string;
+    hotelId: string;
+    geoCode: {
+        latitude: number;
+        longitude: number;
+    };
+    address: {
+        countryCode: string;
+    };
+    amenities: string[];
+    rating: number;
+    lastUpdate: string;
+};
