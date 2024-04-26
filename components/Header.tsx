fc98723a3989c24c89cfd1efce4fe342a16704d3
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-04-26 19:46:56 +0200

    Fix debouncing function for fetching data for IATA codes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,47 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Logo from "./Logo";
 import { FaLocationDot } from "react-icons/fa6";
 import { debounce } from "lodash";
 import { fetchIataCodes } from "../utils/fetchDataHeader";
-
+import { v4 as uuidv4 } from "uuid"
 import { CityType } from "../types/CityType"
+import Image from "next/image";
 
 const Header: React.FC = () => {
 
     const [citiesHeader, setCitiesHeader] = useState<CityType[]>([])
-    // Create a debounced function using useCallback
+
+    const [searchQuery, setSearchQuery] = useState<string>("")
+
+    const [isFetching, setIsFetching] = useState<boolean>(true)
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
     const debounceFetch = useCallback(
-        debounce((searchQuery: string) => {
-            fetchIataCodes(searchQuery);
+        debounce(async (searchQueryInput: string) => {
+            setSearchQuery(searchQueryInput)
         }, 500),
         []
     );
 
+    useEffect(() => {
+        if (searchQuery.length < 3) {
+            return setCitiesHeader([])
+        }
+        const fetchData = async () => {
+            const fetchedData = await fetchIataCodes(searchQuery);
+            setIsFetching(false)
+            fetchedData ? setCitiesHeader(fetchedData) : setCitiesHeader([])
+        }
+        fetchData()
+    }, [searchQuery])
+
     // Function to handle input change
     function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
         // Get the value from the input field
-        const searchQuery = event.target.value;
+        const searchQueryInput = event.target.value;
+        setSearchQuery(searchQueryInput)
         // Call the debounced function with the searchQuery
-        debounceFetch(searchQuery);
+        debounceFetch(searchQueryInput);
     }
 
     return (
@@ -32,7 +50,8 @@ const Header: React.FC = () => {
                 <div className="flex justify-start absolute top-7 left-20">
                     <Logo />
                 </div>
-                <div className="flex items-center gap-4 bg-white py-4 px-3 rounded-lg">
+                <div className="flex items-center gap-4 bg-white py-4 px-3 
+                rounded-lg relative">
                     <FaLocationDot className="text-black text-[1.2rem]" />
                     <input
                         type="text"
@@ -41,6 +60,34 @@ const Header: React.FC = () => {
                         placeholder="Where do you want to go?"
                         onChange={handleChange} // Attach the debounced function to the onChange event
                     />
+
+                    {!isFetching && citiesHeader.length === 0 && searchQuery.length > 2 && (
+
+                        <div className="absolute top-16 left-0 bg-white z-10
+                        w-full py-3 text-center rounded-sm">
+                            <span className="font-bold">
+                                No results found!
+                            </span>
+                        </div>
+                    )}
+
+                    {!isFetching && citiesHeader.length > 0 && (
+                        <div className="absolute top-16 left-0 bg-white z-10
+                            flex flex-col gap-3 w-full rounded-t-md py-5">
+                            {citiesHeader.map(city => (
+                                <div key={uuidv4()} className="flex items-center px-5
+                            gap-5">
+                                    <Image src={`https://countryflagsapi.netlify.app/flag/${city.address.countryCode.toLowerCase()}.svg`}
+                                        alt="" width={200} height={200}
+                                        className="w-10 rounded-sm" />
+                                    <span>
+                                        {city.name}
+                                    </span>
+                                </div>
+                            ))}
+                        </div>
+
+                    )}
                 </div>
             </div>
         </header>
