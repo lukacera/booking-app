fc3d4372a9bddeef3fa7693c081785488e570035
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-04-27 14:51:17 +0200

    Store managment for selected city

---
+++ contexts/SelectedCityHook.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { createContext, useState, ReactNode, Dispatch, SetStateAction, useEffect } from 'react';
+import { CityType } from "../types/CityType"
+
+interface SelectedCityType {
+    selectedCity: CityType,
+    setSelectedCity: Dispatch<SetStateAction<CityType>>
+}
+export const SelectedCityContext = createContext<SelectedCityType>({} as SelectedCityType)
+
+export const SelectedCityProvider: React.FC<{
+    children: ReactNode
+}> = ({ children }) => {
+
+    const [selectedCity, setSelectedCity] = useState<CityType>({} as CityType);
+
+    return (
+        <SelectedCityContext.Provider value={{ selectedCity, setSelectedCity }}>
+            {children}
+        </SelectedCityContext.Provider>
+    );
+};
