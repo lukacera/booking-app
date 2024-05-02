1e1cd152c237445d772a1915d0fa61ea3b87cfdf
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-05-02 18:46:50 +0200

    Update styling for single hotel page

--- app/allhotels/[city]/[hotel]/page.tsx
+++ app/allhotels/[city]/[hotel]/page.tsx
@@ -8,10 +8,16 @@ import { fetchHotel } from "../../../../utils/fetchHotel";
 import { HotelType } from "../../../../types/HotelType";
 import Image from "next/image";
 import InsideOfHotel from "../../../../public/images/insideOfHotel.jpg"
+import OutsideHotel from "../../../../public/images/HotelOutside.jpg"
+import HotelPic from "../../../../public/images/hotel3.jpg"
 import { Nunito_Sans } from "next/font/google";
 
-import { CircularProgressbar, buildStyles } from "react-circular-progressbar"
 import { RatingType } from "../../../../types/RatingType";
+import Ratings from "../../../../components/singleHotelPageComponents/Ratings";
+import Comments from "../../../../components/singleHotelPageComponents/Comments";
+import { FaStar } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
 const nunito = Nunito_Sans({ subsets: ["latin"] })
 const HotelPage: React.FC = () => {
 
@@ -19,6 +25,8 @@ const HotelPage: React.FC = () => {
     const hotel = useParams<{ city: string, hotel: string }>()
 
     const [hotelData, setHotelData] = useState<HotelType>({} as HotelType)
+
+    const [isLoaded, setIsLoaded] = useState<boolean>(false)
 
     const [ratings, setRatings] = useState<RatingType>({} as RatingType)
     // Fetch hotel's data every time hotelID changes in URL
@@ -28,6 +36,7 @@ const HotelPage: React.FC = () => {
                 const data = await fetchHotel(hotel.hotel, hotel.city)
                 setHotelData(data.hotel)
                 data.ratings && setRatings(data.ratings)
+                setIsLoaded(true)
             } catch (error) {
                 console.log("Error occured while setting hotel: " + error)
             }
@@ -36,195 +45,65 @@ const HotelPage: React.FC = () => {
         fetchData()
     }, [hotel])
 
-    return (
-        <>
-            <Header />
-            <main className={`mt-[8.5rem] min-h-[calc(100vh-8.5rem)]
-                bg-white w-[80vw] mx-auto mb-20 grid ${nunito.className}`}>
-                <div className="w-full  h-full flex flex-col
-                gap-10">
-                    {/* Image and name of hotel */}
-                    <div className="flex justify-center gap-40 items-center
-                    py-20">
-                        <div>
-                            <Image alt="" src={InsideOfHotel}
-                                width={200} height={200}
-                                className="w-[22rem] aspect-square rounded-lg shadow-md" />
-                        </div>
-                        <h3 className="font-bold text-2xl">
-                            {hotelData.name}
-                        </h3>
-                    </div>
-                    {/* Container for hotel's ratings */}
-                    <div className="grid grid-cols-[30%_70%] h-full 
-                     py-10">
-                        {/* Container for main overall rating */}
-                        <div className="flex flex-col items-center 
-                        justify-center gap-5 border-r-2 
-                        bg-brown_1 text-white rounded-xl">
-                            <span className="font-bold text-2xl">
-                                Overall rating
-                            </span>
-                            {ratings.overallRating && (
-                                <CircularProgressbar value={ratings.overallRating / 10} />
-                            )}
-                            {!ratings.overallRating && (
-                                <div className="w-80">
-                                    <CircularProgressbar
-                                        value={100}
-                                        minValue={0}
-                                        maxValue={100}
-                                        text={"?"}
-                                        styles={buildStyles({
-                                            pathColor: "white",
-                                            trailColor: "#ebc417",
-                                            textColor: "white"
-                                        })}
-                                    />
+    if (isLoaded) {
+        return (
+            <>
+                <Header />
+                <main className={`mt-[8.5rem] min-h-[calc(100vh-8.5rem)]
+                    bg-white w-[80vw] mx-auto mb-28 grid ${nunito.className}`}>
+                    <div className="w-full  h-full flex flex-col
+                    gap-10">
+                        {/* Image and name of hotel */}
+                        <div className="flex items-center justify-center gap-10
+                        py-20">
+
+                            <div className="flex justify-center 
+                            items-center gap-3">
+                                <Image alt="" src={InsideOfHotel}
+                                    width={280} height={280}
+                                    className="w-80 h-auto rounded-lg shadow-md" />
+                                <div className="flex flex-col gap-5">
+                                    <Image alt="" src={OutsideHotel}
+                                        width={280} height={280}
+                                        className="w-80 h-auto aspect-auto rounded-lg shadow-md" />
+                                    <Image alt="" src={HotelPic}
+                                        width={280} height={280}
+                                        className="w-80 h-auto aspect-auto rounded-lg shadow-md" />
                                 </div>
-                            )}
-                        </div>
-                        {/* Other ratings */}
-                        <div className="grid grid-cols-3 gap-10">
-                            <div className="flex flex-col items-center justify-center gap-5">
-                                <span className="font-bold text-2xl">
-                                    Catering
-                                </span>
-                                {ratings.sentiments?.catering && (
-                                    <CircularProgressbar value={ratings.sentiments.catering / 10} />
-                                )}
-                                {!ratings.sentiments?.catering && (
-                                    <div className="w-40">
-                                        <CircularProgressbar
-                                            value={100}
-                                            minValue={0}
-                                            maxValue={100}
-                                            text={"?"}
-                                            styles={buildStyles({
-                                                pathColor: "#5A3825",
-                                                trailColor: "white"
-                                            })}
-                                        />
-                                    </div>
-                                )}
                             </div>
-                            <div className="flex flex-col items-center justify-center gap-5">
-                                <span className="font-bold text-2xl">
-                                    Facilities
-                                </span>
-                                {ratings.sentiments?.facilities && (
-                                    <CircularProgressbar value={ratings.sentiments.facilities / 10} />
-                                )}
-                                {!ratings.sentiments?.facilities && (
-                                    <div className="w-40">
-                                        <CircularProgressbar
-                                            value={100}
-                                            minValue={0}
-                                            maxValue={100}
-                                            text={"?"}
-                                            styles={buildStyles({
-                                                pathColor: "#5A3825",
-                                                trailColor: "white"
-                                            })}
-                                        />
+                            <div className="flex flex-col gap-10 items-center">
+                                <h3 className="font-bold text-4xl">
+                                    {hotelData.name}
+                                </h3>
+                                <div className="flex items-center gap-5">
+                                    <div className="flex items-center gap-2">
+                                        <span className="font-bold text-[1.3rem]">
+                                            9.5 / 10
+                                        </span>
+                                        <FaStar className="text-gold_1 text-[1.6rem] pb-1" />
                                     </div>
-                                )}
-                            </div>
-                            <div className="flex flex-col items-center justify-center gap-5">
-                                <span className="font-bold text-2xl">
-                                    Internet
-                                </span>
-                                {ratings.sentiments?.internet && (
-                                    <CircularProgressbar value={ratings.sentiments.internet / 10} />
-                                )}
-                                {!ratings.sentiments?.internet && (
-                                    <div className="w-40">
-                                        <CircularProgressbar
-                                            value={100}
-                                            minValue={0}
-                                            maxValue={100}
-                                            text={"?"}
-                                            styles={buildStyles({
-                                                pathColor: "#5A3825",
-                                                trailColor: "white"
-                                            })}
-                                        />
-                                    </div>
-                                )}
-                            </div>
-                            <div className="flex flex-col items-center justify-center gap-5">
-                                <span className="font-bold text-2xl">
-                                    Location
-                                </span>
-                                {ratings.sentiments?.location && (
-                                    <CircularProgressbar value={ratings.sentiments.location / 10} />
-                                )}
-                                {!ratings.sentiments?.location && (
-                                    <div className="w-40">
-                                        <CircularProgressbar
-                                            value={100}
-                                            minValue={0}
-                                            maxValue={100}
-                                            text={"?"}
-                                            styles={buildStyles({
-                                                pathColor: "#5A3825",
-                                                trailColor: "white"
-                                            })}
-                                        />
-                                    </div>
-                                )}
-                            </div>
-                            <div className="flex flex-col items-center justify-center gap-5">
-                                <span className="font-bold text-2xl">
-                                    Staff
-                                </span>
-                                {ratings.sentiments?.staff && (
-                                    <CircularProgressbar value={ratings.sentiments.staff / 10} />
-                                )}
-                                {!ratings.sentiments?.staff && (
-                                    <div className="w-40">
-                                        <CircularProgressbar
-                                            value={100}
-                                            minValue={0}
-                                            maxValue={100}
-                                            text={"?"}
-                                            styles={buildStyles({
-                                                pathColor: "#5A3825",
-                                                trailColor: "white"
-                                            })}
-                                        />
-                                    </div>
-                                )}
-                            </div>
-                            <div className="flex flex-col items-center justify-center gap-5">
-                                <span className="font-bold text-2xl">
-                                    Value
-                                </span>
-                                {ratings.sentiments?.valueForMoney && (
-                                    <CircularProgressbar value={ratings.sentiments.valueForMoney / 10} />
-                                )}
-                                {!ratings.sentiments?.valueForMoney && (
-                                    <div className="w-40">
-                                        <CircularProgressbar
-                                            value={100}
-                                            minValue={0}
-                                            maxValue={100}
-                                            text={"?"}
-                                            styles={buildStyles({
-                                                pathColor: "#5A3825",
-                                                trailColor: "white"
-                                            })}
-                                        />
-                                    </div>
-                                )}
+                                    <span>
+                                        Number of reviews: 52
+                                    </span>
+                                </div>
+                                <div className="flex items-center gap-3 text-[1.2rem]">
+                                    <FaLocationDot className="text-red-500" />
+                                    <span>
+                                        Sunset Avenue, 123
+                                    </span>
+                                </div>
                             </div>
                         </div>
+                        <Comments />
+                        {/* Container for hotel's ratings */}
+                        <Ratings ratings={ratings} />
                     </div>
-                </div>
-            </main>
-            <Footer />
-        </>
-    )
+                </main>
+                <Footer />
+            </>
+        )
+
+    }
 };
 
 export default HotelPage;
