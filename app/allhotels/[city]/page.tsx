f37380f95660ced5ab26cb253b0734df119fec7d
cera04 <152397299+ceraa04@users.noreply.github.com.>
2024-05-04 20:23:54 +0200

    Update responsive for all hotels by city page

--- app/allhotels/[city]/page.tsx
+++ app/allhotels/[city]/page.tsx
@@ -10,14 +10,18 @@ import { fetchHotels } from "../../../utils/fetchHotelsCity";
 import { SelectedCityContext } from "../../../contexts/SelectedCityHook";
 import { Audio } from "react-loader-spinner"
 import SidebarFilters from "../../../components/allHotelsPageComponents/SidebarFilters";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { FaFilter } from "react-icons/fa";
+import FilterSidebarResponsive from "../../../components/allHotelsPageComponents/FilterSidebarResponsive";
 
 const AllHotels = () => {
 
+    // State management for hotels of city
     const [hotels, setHotels] = useState<HotelType[]>([])
 
     const [isLoading, setIsLoading] = useState<boolean>(true)
 
+    // State managment for tracking current state of dropdown menu 
+    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false)
     const { selectedCity } = useContext(SelectedCityContext)
 
     // Get IATA code for selected city from params
@@ -38,49 +42,72 @@ const AllHotels = () => {
 
     return (
         <>
-            <Header />
-            {/* Set mt to 8.5 and calculate height based on height of header */}
-            <div className={`mt-[8.5rem] min-h-[calc(100vh-8.5rem)]
+            {isDropdownOpen && (
+                <>
+                    <Header />
+                    <div className="fixed w-full min-h-screen right-0 top-10 z-40
+                    pb-20">
+                        <FilterSidebarResponsive
+                            selectedFilters={selectedFilters}
+                            setSelectedFilters={setSelectedFilters}
+                            setIsDropdownOpen={setIsDropdownOpen}
+                            hotelsNumber={hotels.length || 0} />
+                    </div>
+                </>
+
+            )}
+            {!isDropdownOpen && (
+                <>
+                    <Header />
+                    {/* Set mt to 8.5 and calculate height based on height of header */}
+                    <div className={`mt-[8.5rem] min-h-[calc(100vh-8.5rem)]
                 bg-white sm:w-[80vw] sm:mx-auto mb-32 
                 ${isLoading && "grid place-items-center"}`}>
-                {isLoading && (
-                    <Audio width={150} height={150} color="#ebc417" />
-                )}
-                {!isLoading && (
-                    <main className="sm:grid sm:grid-cols-[25%_75%]">
-                        {/* Sidebar with all filters */}
-                        <SidebarFilters
-                            setSelectedFilters={setSelectedFilters}
-                            selectedFilters={selectedFilters} />
-                        {/* Main part of page */}
-                        <div className="grid grid-rows-[10rem,auto]">
-                            <div className="relative w-full flex justify-center">
-                                <GiHamburgerMenu className="absolute 
-                                top-7 left-5 text-2xl" />
-                                <h2 className="font-bold text-2xl flex items-center gap-1">
-                                    Hotels in
-                                    <span className="text-brown_1 italic">
-                                        {selectedCity}
-                                    </span>
-                                </h2>
-                            </div>
-                            <div className="sm:w-[90%] sm:mx-auto mb-20 flex flex-col gap-10">
-                                {hotels?.length > 0 && hotels.map(hotel => (
-                                    <SingleHotel hotel={hotel} key={uuidv4()} />
-                                ))}
-                                {hotels?.length === 0 && (
-                                    <div className="flex justify-center items-start pt-32 h-full">
-                                        <span className="font-bold text-4xl text-brown_1">
-                                            No hotels found!
-                                        </span>
+                        {isLoading && (
+                            <Audio width={150} height={150} color="#ebc417" />
+                        )}
+                        {!isLoading && (
+                            <main className="sm:grid sm:grid-cols-[25%_75%]">
+                                {/* Sidebar with all filters */}
+                                <SidebarFilters
+                                    setSelectedFilters={setSelectedFilters}
+                                    selectedFilters={selectedFilters} />
+                                {/* Main part of page */}
+                                <div className="grid grid-rows-[10rem,auto]">
+                                    <div className="relative w-full flex justify-center">
+                                        <FaFilter className="absolute
+                                    top-10 left-5 sm:hidden flex items-center gap-2 
+                                    text-2xl cursor-pointer" title="Filters"
+                                            onClick={() => setIsDropdownOpen(true)} />
+                                        <h2 className="font-bold text-2xl flex items-center gap-1">
+                                            Hotels in
+                                            <span className="text-brown_1 italic">
+                                                {selectedCity}
+                                            </span>
+                                        </h2>
                                     </div>
-                                )}
-                            </div>
-                        </div>
-                    </main>
-                )}
-            </div>
-            <Footer />
+                                    <div className="sm:w-[90%] sm:mx-auto mb-20 flex flex-col gap-10">
+                                        {hotels?.length > 0 && hotels.map(hotel => (
+                                            <SingleHotel hotel={hotel} key={uuidv4()} />
+                                        ))}
+                                        {hotels?.length === 0 && (
+                                            <div className="flex justify-center 
+                                    items-start pt-32 h-full">
+                                                <span className="font-bold text-4xl 
+                                        text-brown_1">
+                                                    No hotels found!
+                                                </span>
+                                            </div>
+                                        )}
+                                    </div>
+                                </div>
+                            </main>
+                        )}
+                    </div>
+                    <Footer />
+
+                </>
+            )}
         </>
     )
 };
